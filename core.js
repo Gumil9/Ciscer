947b0a27e6916df46dfb86b9998dfb380e4fd7da
Gumil9 <andrew.gumil95@gmail.com>
2016-08-14 18:04:59 +0300

    Added opacity property for div block

--- core.js
+++ core.js
@@ -116,6 +116,7 @@ function Label() {
     div.style.color = COLOR;
     div.style.zIndex = 1000000;
     div.style.display = 'block';
+    div.style.opacity = 0;
     document.body.appendChild(div);
     var above = false;
 
