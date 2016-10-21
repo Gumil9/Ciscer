6ed19a47f0d9be74497f95ed2d5c6d1671364a7c
Gumil9 <andrew.gumil95@gmail.com>
2016-10-21 14:45:25 +0300

    Code refactoring

--- core.js
+++ core.js
@@ -7,102 +7,108 @@ var label = new Label();
 var selection = new Selection();
 var answerSigner = new AnswerSigner(tests);
 
-document.onclick = function handleClick() {
-    label.setText(tests.getAnswer(window.getSelection().toString()));
-};
+document.addEventListener('click', handleClick);
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener("wheel", handleWheel);
 
-document.body.onkeydown = function handleKeyCombination(e) {
+function handleClick() {
+    label.setText(tests.getAnswer(window.getSelection().toString()));
+}
+
+function handleKeyDown(event) {
 
     //Ctrl + Alt + (↑ or ↓)
-    if (e.ctrlKey && e.altKey && (e.keyCode == 38 || e.keyCode == 40)) {
+    if (event.ctrlKey && event.altKey && (event.keyCode == 38 || event.keyCode == 40)) {
         label.changeDisposition();
     }
 
     //Ctrl + Alt + ←
-    if (e.ctrlKey && e.altKey && e.keyCode == 37) {
+    if (event.ctrlKey && event.altKey && event.keyCode == 37) {
         label.setText(prompts.getPrevious());
     }
 
     //Ctrl + Alt + →
-    if (e.ctrlKey && e.altKey && e.keyCode == 39) {
+    if (event.ctrlKey && event.altKey && event.keyCode == 39) {
         label.setText(prompts.getNext());
     }
 
     //Ctrl + Shift
-    if (e.ctrlKey && e.shiftKey) {
+    if (event.ctrlKey && event.shiftKey) {
         selection.changeSelectionColor();
     }
 
     //Ctrl + Space
-    if (e.ctrlKey && e.keyCode == 32) {
+    if (event.ctrlKey && event.keyCode == 32) {
         answerSigner.run();
     }
+}
 
-};
-
-document.body.addEventListener("wheel", function wheelHandler(e) {
-    if (e.ctrlKey && e.altKey) {
-        var delta = e.deltaY || e.detail || e.wheelDelta;
+function handleWheel(event) {
+    if (event.ctrlKey && event.altKey) {
+        var delta = event.deltaY || event.detail || event.wheelDelta;
         if (delta > 0) {
             label.setText(prompts.getNext());
         } else if (delta < 0) {
             label.setText(prompts.getPrevious());
         }
     }
-});
+}
 
 /*Stores and finds answers to multiple choice and fill-in-the-blank questions*/
 function Tests() {
-    var arr = []; //array with questions and answers
+    var testsArray = []; //array with questions and answers
     this.add = function (question) {
         arguments.slice = [].slice;
-        var answer = arguments.slice(1);
-        arr.push(new Test(question, answer));
+        var answers = arguments.slice(1);
+        testsArray.push(new Test(question, answers));
 
-        function Test(question, answer) {
+        function Test(question, answers) {
             this.question = question;
-            this.answer = answer;
+            this.answers = answers;
         }
     };
 
     /*
-     Following constant defines minimum length of question (selected text) to search answers to.
-     It is required for secrecy with outside users.
+     * Following constant defines minimum length of question (selected text) to search answers to.
+     * It is required for secrecy with outside users.
      */
     var QUESTION_MIN_LENGTH = 10;
     this.getAnswer = function (question) {
         if (question.length < QUESTION_MIN_LENGTH) {
             return '';
         }
-        for (var i = 0; i < arr.length; i++) {
-            if (~arr[i].question.indexOf(question)) {
-                return arr[i].answer.join('<br>');
+        var answer;
+        testsArray.forEach(function (test) {
+            if (~test.question.indexOf(question)) {
+                answer = test.answers.join('<br>');
             }
-        }
-        return 'not found';
+        });
+        return 'not found' || answer;
     };
     this.getTests = function () {
-        return arr;
+        return testsArray;
     }
 }
 
-/*Stores prompts for drag-and-drop (matching) questions and Packet Tracer Labs*/
+/*
+ * Stores prompts for drag-and-drop (matching) questions and Packet Tracer Labs
+ */
 function Prompts() {
-    var array = [];
-    var index = -1;
+    var promptsArray = [];
+    var selectedIndex = -1;
 
     this.getNext = function () {
-        index = (index + 1) % array.length;
-        return array[index];
+        selectedIndex = (selectedIndex + 1) % promptsArray.length;
+        return promptsArray[selectedIndex];
     };
 
     this.getPrevious = function () {
-        index = (--index >= 0) ? index : index + array.length;
-        return array[index];
+        selectedIndex = (--selectedIndex >= 0) ? selectedIndex : selectedIndex + promptsArray.length;
+        return promptsArray[selectedIndex];
     };
 
     this.add = function (prompt) {
-        array.push(prompt);
+        promptsArray.push(prompt);
     };
 
 }
@@ -117,7 +123,7 @@ function Label() {
     div.style.zIndex = 1000000;
     div.style.display = 'block';
     document.body.appendChild(div);
-    var above = false;
+    var topPosition = false;
 
     this.setText = function (answer) {
         div.innerHTML = answer;
@@ -134,47 +140,50 @@ function Label() {
     };
 
     this.changeDisposition = function () {
-        if (above) {
-            above = false;
+        if (topPosition) {
+            topPosition = false;
             this.displayBelow();
         } else {
-            above = true;
+            topPosition = true;
             this.displayAbove();
         }
     };
 }
 
 function AnswerSigner(tests) {
-    var ANSWER_MIN_LENGTH = 5; //This constant defines minimum length of answer to mark.
+    var ANSWER_MIN_LENGTH = 5; //This constant defines minimum length of answers to mark.
 
     this.run = function () {
-        var label = document.getElementsByTagName('label');
+        var labelElements = document.getElementsByTagName("label");
 
-        var arr = tests.getTests();
-        arr.sort(function compareByLength(a, b) {
-            return b.answer[0].length - a.answer[0].length;
+        var testsArray = tests.getTests();
+        testsArray.sort(function compareByLength(a, b) {
+            return b.answers[0].length - a.answers[0].length;
         });
 
-        for (var i = 0; i < arr.length; i++) {
-            var answer = arr[i].answer;
-            for (var j = 0; j < answer.length; j++) {
-                findAndSign(answer[j]);
-            }
-        }
+        testsArray.forEach(function (test) {
+            var answers = test.answers;
+            answers.forEach(function (answer) {
+                findAndSign(answer);
+            });
+        });
+
         function findAndSign(answer) {
             if (answer.length < ANSWER_MIN_LENGTH) {
                 return;
             }
             var regExp = new RegExp(answer, 'i');
-            for (var i = 0; i < label.length; i++) {
-                if (label[i].textContent == answer) {
-                    label[i].innerHTML = '<span style="color: ' + COLOR + ' !important;">·</span>' + label[i].innerHTML;
+
+            Array.prototype.forEach.call(labelElements, function (element) {
+                if (element.textContent == answer) {
+                    console.log(element);
+                    element.innerHTML = '<span style="color: ' + COLOR + ' !important;">·</span>' + element.innerHTML;
                 }
-                if (label[i].innerHTML.indexOf('·') == -1) {
-                    label[i].innerHTML = label[i].innerHTML.replace(regExp,
+                if (element.innerHTML.indexOf('·') == -1) {
+                    element.innerHTML = element.innerHTML.replace(regExp,
                         '<span style="color: ' + COLOR + ' !important;">·</span>' + answer);
                 }
-            }
+            });
         }
     };
 }
