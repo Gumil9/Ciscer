effee5b20ea3ac2cbb22b7eb75948786d3fd8eb3
Nickolay Gaiduchenko <yozhiknick@mail.ru>
2017-09-29 10:02:29 +0300

    Added CCNA1-Chapter8

--- answers.js
+++ answers.js
@@ -132,6 +132,60 @@ add('How many bits are in an IPv4 address?',
 
 
 
+// CCNA1 Introduction to Networks v6.0 Chapter 8
+add('What is a result of connecting two or more switches together?',
+    'The size of the broadcast domain is increased.');
+add('Refer to the exhibit. How many broadcast domains are there?',
+    '4');
+add('What are two reasons a network administrator might want to create subnets? (Choose two.)',
+    'improves network performance',
+    'easier to implement security policies');
+add('Refer to the exhibit. A company uses the address block of 128.107.0.0/16 for its network. What subnet mask would provide the maximum number of equal size subnets while providing enough host addresses for each subnet in the exhibit?',
+    '255.255.255.128');
+add('Refer to the exhibit. The network administrator has assigned the LAN of LBMISS an address range of 192.168.10.0. This address range has been subnetted using a /29 prefix. In order to accommodate a new building, the technician has decided to use the fifth subnet for configuring the new network (subnet zero is the first subnet). By company policies, the router interface is always assigned the first usable host address and the workgroup server is given the last usable host address. Which configuration should be entered into the properties of the workgroup server to allow connectivity to the Internet?',
+    'IP address: 192.168.10.38 subnet mask: 255.255.255.248, default gateway: 192.168.10.33');
+add('If a network device has a mask of /28, how many IP addresses are available for hosts on this network?',
+    '14');
+add('Which subnet mask would be used if 5 host bits are available?',
+    '255.255.255.224');
+add('How many host addresses are available on the network 172.16.128.0 with a subnet mask of 255.255.252.0?',
+    '1022');
+add('How many bits must be borrowed from the host portion of an address to accommodate a router with five connected networks?',
+    'three');
+add('A network administrator wants to have the same network mask for all networks at a particular small site. The site has the following networks and number of devices: IP phones – 22 addresses PCs – 20 addresses needed Printers – 2 addresses needed Scanners – 2 addresses needed The network administrator has deemed that 192.168.10.0/24 is to be the network used at this site. Which single subnet mask would make the most efficient use of the available addresses to use for the four subnetworks?',
+    '255.255.255.224');
+add('A company has a network address of 192.168.1.64 with a subnet mask of 255.255.255.192. The company wants to create two subnetworks that would contain 10 hosts and 18 hosts respectively. Which two networks would achieve that? (Choose two.)',
+    '192.168.1.64/27',
+    '192.168.1.96/28');
+add('A network administrator is variably subnetting a network. The smallest subnet has a mask of 255.255.255.248. How many usable host addresses will this subnet provide?',
+    '6');
+add('Refer to the exhibit. Given the network address of 192.168.5.0 and a subnet mask of 255.255.255.224, how many total host addresses are unused in the assigned subnets?',
+    '72');
+add('Refer to the exhibit. Considering the addresses already used and having to remain within the 10.16.10.0/24 network range, which subnet address could be assigned to the network containing 25 hosts?',
+    '10.16.10.64/27');
+add('A network administrator needs to monitor network traffic to and from servers in a data center. Which features of an IP addressing scheme should be applied to these devices?',
+    'predictable static IP addresses for easier identification');
+add('Which two reasons generally make DHCP the preferred method of assigning IP addresses to hosts on large networks? (Choose two.)',
+    'It eliminates most address configuration errors.',
+    'It reduces the burden on network support staff.');
+add('A DHCP server is used to assign IP addresses dynamically to the hosts on a network. The address pool is configured with 192.168.10.0/24. There are 3 printers on this network that need to use reserved static IP addresses from the pool. How many IP addresses in the pool are left to be assigned to other hosts?',
+    '251');
+add('Refer to the exhibit. A company is deploying an IPv6 addressing scheme for its network. The company design document indicates that the subnet portion of the IPv6 addresses is used for the new hierarchical network design, with the site subsection to represent multiple geographical sites of the company, the sub-site section to represent multiple campuses at each site, and the subnet section to indicate each network segment separated by routers. With such a scheme, what is the maximum number of subnets achieved per sub-site?',
+    '16');
+add('What is the prefix for the host address 2001:DB8:BC15:A:12AB::1/64?',
+    '2001:DB8:BC15:A');
+add('Consider the following range of addresses: 2001:0DB8:BC15:00A0:0000:: 2001:0DB8:BC15:00A1:0000:: 2001:0DB8:BC15:00A2:0000:: … 2001:0DB8:BC15:00AF:0000:: The prefix-length for the range of addresses is',
+    '/60');
+add('Match the subnetwork to a host address that would be included within the subnetwork. (Not all options are used.)',
+    '192.168.1.68 -> 192.168.1.64/27',
+    '192.168.1.48 -> 192.168.1.32/27',
+    '192.168.1.121 -> 192.168.1.96/27');
+add('Refer to the exhibit. Match the network with the correct IP address and prefix that will satisfy the usable host addressing requirements for each network. (Not all options are used.)',
+    '192.168.0.192 /27 -> Network C',
+    '192.168.0.0 /25   -> Network A',
+    '192.168.0.224 /30 -> Network D',
+    '192.168.0.128 /26 -> Network B');
+
 
 /*Prompts for matching questions and Packet Tracer Labs*/
 add = prompts.add;
