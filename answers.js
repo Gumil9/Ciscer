1df24a047bb3e909bcf179b259e91fd5208036e7
Roman Anufriev <mrspeedstalker@gmail.com>
2017-10-28 11:10:23 +0300

    Added CCNA3 Chapter 7, deleted CCNA3 Chapter 6

--- answers.js
+++ answers.js
@@ -6,59 +6,63 @@ var add = tests.add;
  * add(question, answer0 [, answer1, ...]);
  */
 
-// CCNA3 Scaling Networks v6.0 Chapter 6
-add('When an EIGRP-enabled router uses a password to accept routes from other EIGRP-enabled routers, which mechanism is used?',
-    'EIGRP authentication');
-add('What is the purpose of using protocol-dependent modules in EIGRP?',
-    'to accommodate routing of different network layer protocols');
-add('If all router Ethernet interfaces in an EIGRP network are configured with the default EIGRP timers, how long will a router wait by default to receive an EIGRP packet from its neighbor before declaring the neighbor unreachable?',
-    '15 seconds');
-add('Which statement describes a characteristic of the delivery of EIGRP update packets?',
-    'EIGRP uses a reliable delivery protocol to send all update packets.');
-add('Which two EIGRP packet types are sent with unreliable delivery? (Choose two.)',
-    'hello',
-    'acknowledgment');
-add('Which destination MAC address is used when a multicast EIGRP packet is encapsulated into an Ethernet frame?',
-    '01-00-5E-00-00-0A');
-add('What is identified within the opcode of an EIGRP packet header?',
-    'the EIGRP message type that is being sent or received from a neighbor');
-add('An administrator issues the router eigrp 100 command on a router. What is the number 100 used for?',
-    'as the autonomous system number');
-add('Why would a network administrator use a wildcard mask in the network command when configuring a router to use EIGRP?',
-    'to exclude some interfaces from the EIGRP process');
-add('What information does EIGRP maintain within the routing table?',
-    'only successors');
-add('Which table is used by EIGRP to store all routes that are learned from EIGRP neighbors?',
-    'the topology table');
-add('Which command or commands must be entered on a serial interface of a Cisco router to restore the bandwidth to the default value of that specific router interface?',
-    'no bandwidth');
-add('Which three metric weights are set to zero by default when costs in EIGRP are being calculated? (Choose three.)',
-    'k2',
-    'k4',
-    'k5');
-add('A new network administrator has been asked to verify the metrics that are used by EIGRP on a Cisco device. Which two EIGRP metrics are measured by using static values on a Cisco device? (Choose two.)',
-    'bandwidth',
-    'delay');
-add('Which two networks contain feasible successors? (Choose two.)',
-    '192.168.71.0',
-    '10.44.100.252');
-add('A network administrator wants to verify the default delay values for the interfaces on an EIGRP-enabled router. Which command will display these values?',
-    'show interfaces');
-add('R3 has two possible paths to the 172.16.99.0 network. What is the reported distance of the feasible successor route?',
-    '2816');
-add('R2 has two possible paths to the 192.168.10.4 network. What would make the alternate route meet the feasibility condition?',
-    'a reported distance less than 3523840');
-add('Which two factors does an EIGRP router use to determine that a route to a remote network meets the feasible condition and is therefore loop-free? (Choose two.)',
-    'the reported distance on a neighbor router',
-    'the feasible distance on the local router');
-add('Which configuration is necessary to ensure successful operation of EIGRP for IPv6?',
-    'The no shutdown command is required within the router configuration mode.');
-add('EIGRP uses the protocol to deliver EIGRP packets to neighbors.',
-    'RTP');
-add('In an EIGRP topology table, a route that is in a/an state will cause the Diffusing Update Algorithm to send EIGRP queries that ask other routers for a path to this network.',
-    'active');
-add('Which code is displayed on the web server?',
-    'Complete');
+// CCNA3 Scaling Networks v6.0 Chapter 7
+add('Router R3 is receiving multiple routes through the EIGRP routing protocol. Which statement is true about the implementation of summarization in this network?',
+    'Automatic summarization is disabled on R3.');
+add('Which two routes will be advertised to the router ISP if autosummarization is disabled? (Choose two.)',
+    '10.1.2.0/24',
+    '10.1.4.0/30');
+add('What is the purpose of a Null0 route in the routing table?',
+    'to prevent routing loops');
+add('All networks are active in the same EIGRP routing domain. When the auto-summary command is issued on R3, which two summary networks will be calculated on R3? (Choose two.)',
+    '192.168.10.0/24',
+    '172.16.0.0/16');
+add('Which three statements are advantages of using automatic summarization? (Choose three.)',
+    'It reduces the frequency of routing updates.',
+    'It ensures that traffic for multiple subnets uses one path through the internetwork.',
+    'It decreases the number of entries in the routing table.');
+add('Assuming that EIGRP is enabled on both routers and automatic summarization is enabled, what must be configured to ensure that R1 will be able to reach the 2.2.2.0/24 network?',
+    'Use the command no auto-summary to disable automatic summarization.');
+add('Which statement is supported by the output?',
+    'A default route is being learned through an external process.');
+add('Which two steps must be taken in order to send a default route to other EIGRP routers? (Choose two.)',
+    'Configure a default route.',
+    'Redistribute the default route.');
+add('What is the administrative distance of externally learned EIGRP routes?',
+    '170');
+add('Two routers, R1 and R2, share a 64 kb/s link. An administrator wants to limit the bandwidth used by EIGRP between these two routers to 48 kb/s. Which command is used on both routers to configure the new bandwidth setting?',
+    'ip bandwidth-percent eigrp 100 75');
+add('Which two conclusions can be drawn from the exhibited configuration? (Choose two.)',
+    'The configuration supports unequal-cost load balancing.',
+    'Any EIGRP-learned route with a metric less than 3 times the successor metric will be installed in the local routing table.');
+add('Which routing protocol supports unequal-cost load balancing on Cisco routers?',
+    'EIGRP');
+add('By default, how many equal-cost routes to the same destination network will EIGRP install in the routing table?',
+    '4');
+add('What is the default maximum amount of bandwidth that can be used for exchanging EIGRP messages on an EIGRP-configured interface?',
+    '50%');
+add('An administrator wants EIGRP on Router1 to load balance traffic to network 2001:db8:11:10::/64 across two interfaces. Currently traffic is using only interface GigabitEthernet0/1. A second route, not in the routing table, is available with a metric of 264000. What value is needed in the variance command to make EIGRP put the second route into the routing table?',
+    '11');
+add('When a Cisco router is configured with fast-switching, how are packets distributed over equal-cost paths?',
+    'on a per-destination basis');
+add('Which command can be issued on a router to verify that automatic summarization is enabled?',
+    'show ip protocols');
+add('After the configuration shown is applied on router R1, the exhibited status message is displayed. Router R1 is unable to form a neighbor relationship with R2 on the serial 0/1/0 interface. What is the most likely cause of this problem?',
+    'The IPv4 address configured on the neighbor that is connected to R1 serial 0/1/0 is incorrect.');
+add('Routers R1 and R2 are directly connected via their serial interfaces and are both running the EIGRP routing protocol. R1 and R2 can ping the directly connected serial interface of their neighbor, but they cannot form an EIGRP neighbor adjacency.',
+    'Configure both routers with the same EIGRP autonomous system number.');
+add('Considering that R2, R3, and R4 are correctly configured, why did R1 not establish an adjacency with R2, R3, and R4?',
+    'because the Fa0/0 interface of R1 is declared as passive for EIGRP');
+add('In which scenario will the use of EIGRP automatic summarization cause inconsistent routing in a network?',
+    'when the routers in an IPv4 network are connected to discontiguous networks with automatic summarization enabled');
+add('Remote users are experiencing connectivity problems when attempting to reach hosts in the 172.21.100.0 /24 network. Using the output in the exhibit, what is the most likely cause of the connectivity problem?',
+    'The GigabitEthernet 0/1 interface is not participating in the EIGRP process.');
+add('command causes an EIGRP router to stop sending hello packets through an interface.',
+    'passive-interface');
+add('The command to propagate the default route from the router Border to the rest of the EIGRP domain is ',
+    'redistribute static');
+add('R1 and R2 could not establish an EIGRP adjacency. What is the problem?',
+    'EIGRP is down on R1.');
 
 /*Prompts for matching questions and Packet Tracer Labs*/
 add = prompts.add;
@@ -68,6 +72,4 @@ add = prompts.add;
  * add('helper label');
  */
 
-// CCNA3 Scaling Networks v6.0 Chapter 6
-add('1 > eigrp, 2 > IPv4 on loopback, 3 > IPv4 on active');
-add('IPv4 > routable, IPv6 > link-local, both > 32-bit');
+// CCNA3 Scaling Networks v6.0 Chapter 7
--- answers_archive.js
+++ answers_archive.js
@@ -5045,3 +5045,62 @@ add('What is a disadvantage of deploying OSPF in a large single area routing env
 
 add('adv: event, building, fast');
 add('disadv: bandwidth, memory, CPU');
+
+
+// CCNA3 Scaling Networks v6.0 Chapter 6
+add('When an EIGRP-enabled router uses a password to accept routes from other EIGRP-enabled routers, which mechanism is used?',
+    'EIGRP authentication');
+add('What is the purpose of using protocol-dependent modules in EIGRP?',
+    'to accommodate routing of different network layer protocols');
+add('If all router Ethernet interfaces in an EIGRP network are configured with the default EIGRP timers, how long will a router wait by default to receive an EIGRP packet from its neighbor before declaring the neighbor unreachable?',
+    '15 seconds');
+add('Which statement describes a characteristic of the delivery of EIGRP update packets?',
+    'EIGRP uses a reliable delivery protocol to send all update packets.');
+add('Which two EIGRP packet types are sent with unreliable delivery? (Choose two.)',
+    'hello',
+    'acknowledgment');
+add('Which destination MAC address is used when a multicast EIGRP packet is encapsulated into an Ethernet frame?',
+    '01-00-5E-00-00-0A');
+add('What is identified within the opcode of an EIGRP packet header?',
+    'the EIGRP message type that is being sent or received from a neighbor');
+add('An administrator issues the router eigrp 100 command on a router. What is the number 100 used for?',
+    'as the autonomous system number');
+add('Why would a network administrator use a wildcard mask in the network command when configuring a router to use EIGRP?',
+    'to exclude some interfaces from the EIGRP process');
+add('What information does EIGRP maintain within the routing table?',
+    'only successors');
+add('Which table is used by EIGRP to store all routes that are learned from EIGRP neighbors?',
+    'the topology table');
+add('Which command or commands must be entered on a serial interface of a Cisco router to restore the bandwidth to the default value of that specific router interface?',
+    'no bandwidth');
+add('Which three metric weights are set to zero by default when costs in EIGRP are being calculated? (Choose three.)',
+    'k2',
+    'k4',
+    'k5');
+add('A new network administrator has been asked to verify the metrics that are used by EIGRP on a Cisco device. Which two EIGRP metrics are measured by using static values on a Cisco device? (Choose two.)',
+    'bandwidth',
+    'delay');
+add('Which two networks contain feasible successors? (Choose two.)',
+    '192.168.71.0',
+    '10.44.100.252');
+add('A network administrator wants to verify the default delay values for the interfaces on an EIGRP-enabled router. Which command will display these values?',
+    'show interfaces');
+add('R3 has two possible paths to the 172.16.99.0 network. What is the reported distance of the feasible successor route?',
+    '2816');
+add('R2 has two possible paths to the 192.168.10.4 network. What would make the alternate route meet the feasibility condition?',
+    'a reported distance less than 3523840');
+add('Which two factors does an EIGRP router use to determine that a route to a remote network meets the feasible condition and is therefore loop-free? (Choose two.)',
+    'the reported distance on a neighbor router',
+    'the feasible distance on the local router');
+add('Which configuration is necessary to ensure successful operation of EIGRP for IPv6?',
+    'The no shutdown command is required within the router configuration mode.');
+add('EIGRP uses the protocol to deliver EIGRP packets to neighbors.',
+    'RTP');
+add('In an EIGRP topology table, a route that is in a/an state will cause the Diffusing Update Algorithm to send EIGRP queries that ask other routers for a path to this network.',
+    'active');
+add('Which code is displayed on the web server?',
+    'Complete');
+
+// CCNA3 Scaling Networks v6.0 Chapter 6
+add('1 > eigrp, 2 > IPv4 on loopback, 3 > IPv4 on active');
+add('IPv4 > routable, IPv6 > link-local, both > 32-bit');
