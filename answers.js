c9110201c3522e85aa7984c69769f885ba2dda3e
Yozh2 <yozhiknick@mail.ru>
2017-11-03 09:34:27 +0300

    Added CCNA2 Chapters 3, 4

--- answers.js
+++ answers.js
@@ -64,6 +64,113 @@ add('The command to propagate the default route from the router Border to the re
 add('R1 and R2 could not establish an EIGRP adjacency. What is the problem?',
     'EIGRP is down on R1.');
 
+
+// CCNA2 Routing and Switching v6.0 Chapter 3
+add('Which dynamic routing protocol was developed to interconnect different Internet service providers?',
+    'BGP');
+add('Which routing protocol is limited to smaller network implementations because it does not accommodate growth for larger networks?',
+    'RIP');
+add('What two tasks do dynamic routing protocols perform? (Choose two.)',
+    'update and maintain routing tables',
+    'network discovery');
+add('When would it be more beneficial to use a dynamic routing protocol instead of static routing?',
+    'on a network where there is a lot of topology changes');
+add('When would it be more beneficial to use static routing instead of dynamic routing protocols?',
+    'on a network where dynamic updates would pose a security risk');
+add('What is a purpose of the network command when configuring RIPv2 as the routing protocol?',
+    'It identifies the interfaces that belong to a specified network.');
+add('A network administrator configures a static route on the edge router of a network to assign a gateway of last resort. How would a network administrator configure the edge router to automatically share this route within RIP?',
+    'Use the default-information originate command.');
+add('What is the purpose of the passive-interface command?',
+    'allows a router to receive routing updates on an interface but not send updates via that interface');
+add('Which route would be automatically created when a router interface is activated and configured with an IP address?',
+    'C 192.168.0.0/24 is directly connected, FastEthernet 0/0');
+add('Refer to the exhibit. Which two types of routes could be used to describe the 192.168.200.0/30 route? (Choose two.)',
+    'ultimate route',
+    'level 2 child route');
+add('What occurs next in the router lookup process after a router identifies a destination IP address and locates a matching level 1 parent route?',
+    'The level 2 child routes are examined.');
+add('Which route would be used to forward a packet with a source IP address of 192.168.10.1 and a destination IP address of 10.1.1.1?',
+    'O 10.1.1.0/24 [110/65] via 192.168.200.2, 00:01:20, Serial0/1/0');
+add('Which two requirements are used to determine if a route can be considered as an ultimate route in a router’s routing table? (Choose two.)',
+    'contain an exit interface',
+    'contain a next-hop IP address');
+add('What is a disadvantage of using dynamic routing protocols?',
+    'They send messages about network status insecurely across networks by default.');
+add('Which two statements are true regarding classless routing protocols? (Choose two.)',
+    'sends subnet mask information in routing updates',
+    'allows for use of both 192.168.1.0/30 and 192.168.1.16/28 subnets in the same topology');
+add('Refer to the exhibit. Based on the partial output from the show ip route command, what two facts can be determined about the RIP routing protocol? (Choose two.)',
+    'RIP version 2 is running on this router and its RIP neighbor.',
+    'The command no auto-summary has been used on the RIP neighbor router.');
+add('While configuring RIPv2 on an enterprise network, an engineer enters the command network 192.168.10.0 into router configuration mode. What is the result of entering this command?',
+    'The interface of the 192.168.10.0 network is sending only version 2 updates.');
+add('A destination route in the routing table is indicated with a code D. Which kind of route entry is this?',
+    'a route dynamically learned through the EIGRP routing protocol');
+add('Refer to the exhibit. Which interface will be the exit interface to forward a data packet with the destination IP address 172.16.0.66?',
+    'Serial0/0/1');
+add('Which type of route will require a router to perform a recursive lookup?',
+    'an ultimate route that is using a next hop IP address on a router that is not using CEF');
+add('Which route is the best match for a packet entering a router with a destination address of 10.16.0.2?',
+    'S 10.16.0.0/24 [1/0] via 192.168.0.9');
+add('A router is configured to participate in multiple routing protocol: RIP, EIGRP, and OSPF. The router must send a packet to network 192.168.14.0. Which route will be used to forward the traffic?',
+    'a 192.168.14.0/26 route that is learned via RIP');
+add('What is different between IPv6 routing table entries compared to IPv4 routing table entries?',
+    'By design IPv6 is classless so all routes are effectively level 1 ultimate routes.');
+
+
+// CCNA2 Routing and Switching v6.0 Chapter 4
+add('A network designer must provide a rationale to a customer for a design which will move an enterprise from a flat network topology to a hierarchical network topology. Which two features of the hierarchical design make it the better choice? (Choose two.)',
+    'easier to provide redundant links to ensure higher availability',
+    'simpler deployment for additional switch equipment');
+add('What is a collapsed core in a network design?',
+    'a combination of the functionality of the distribution and core layers');
+add('What is a definition of a two-tier LAN network design?',
+    'distribution and core layers collapsed into one tier, and the access layer on a separate tier');
+add('What is a basic function of the Cisco Borderless Architecture distribution layer?',
+    'aggregating Layer 3 routing boundaries');
+add('Which two previously independent technologies should a network administrator attempt to combine after choosing to upgrade to a converged network infrastructure? (Choose two.)',
+    'user data traffic',
+    'VoIP phone traffic');
+add('A local law firm is redesigning the company network so that all 20 employees can be connected to a LAN and to the Internet. The law firm would prefer a low cost and easy solution for the project. What type of switch should be selected?',
+    'fixed configuration');
+add('What are two advantages of modular switches over fixed-configuration switches? (Choose two.)',
+    'increased scalability',
+    'need for fewer power outlets');
+add('Which type of address does a switch use to build the MAC address table?',
+    'source MAC address');
+add('Which network device can be used to eliminate collisions on an Ethernet network?',
+    'switch');
+add('What two criteria are used by a Cisco LAN switch to decide how to forward Ethernet frames? (Choose two.)',
+    'ingress port',
+    'destination MAC address');
+add('Refer to the exhibit. Consider that the main power has just been restored. PC3 issues a broadcast IPv4 DHCP request. To which port will SW1 forward this request?​',
+    'to Fa0/1, Fa0/2, and Fa0/3 only');
+add('What is one function of a Layer 2 switch?',
+    'determines which interface is used to forward a frame based on the destination MAC address');
+add('Refer to the exhibit. How is a frame sent from PCA forwarded to PCC if the MAC address table on switch SW1 is empty?',
+    'SW1 floods the frame on all ports on SW1, excluding the port through which the frame entered the switch.');
+add('A small publishing company has a network design such that when a broadcast is sent on the LAN, 200 devices receive the transmitted broadcast. How can the network administrator reduce the number of devices that receive broadcast traffic?',
+    'Segment the LAN into smaller LANs and route between them.');
+add('Refer to the exhibit. How many broadcast domains are displayed?',
+    '8');
+add('Which solution would help a college alleviate network congestion due to collisions?',
+    'a high port density switch');
+add('Which network device can serve as a boundary to divide a Layer 2 broadcast domain?',
+    'router');
+add('What is the destination address in the header of a broadcast frame?',
+    'FF-FF-FF-FF-FF-FF');
+add('Which statement describes a result after multiple Cisco LAN switches are interconnected?',
+    'The broadcast domain expands to all switches.');
+add('What does the term “port density” represent for an Ethernet switch?',
+    'the number of available ports');
+add('What are two reasons a network administrator would segment a network with a Layer 2 switch? (Choose two.)',
+    'to enhance user bandwidth',
+    'to isolate traffic between segments');
+add('A converged network is one that uses the same infrastructure to carry voice, data, and video signals.',
+    'converged');
+
+
 /*Prompts for matching questions and Packet Tracer Labs*/
 add = prompts.add;
 
@@ -71,3 +178,22 @@ add = prompts.add;
  * template:
  * add('helper label');
  */
+
+ // CCNA2 Routing and Switching v6.0 Chapter 3
+ add('data structures > tables or databases that are stored in RAM');
+ add('routing protocol messages > exchanges routing information and maintains accurate information about networks');
+ add('algorithm > a finite list of steps used to determine the best path');
+ add('static routing > "typically used on stub networks" and "less routing overhead"');
+ add('dynamic routing > "new networkd are added automatically to the routing table" and "best choice for large networks"');
+
+ // CCNA2 Routing and Switching v6.0 Chapter 4
+ add('fkexibility < allows intelligent traffic load sharing by using all network resources');
+ add('hierarchical < facilitates understanding the role of each device at every tier, simplifies deployment, operation, management, and reduces fault domains at every tier ');
+ add('modularity < allows seamless network expansion and integrated service enablement on an on-demand basis');
+ add('resiliency < satisfies user expectations for keeping the network always on');
+
+ add('Access layer > "represents the network edge" and "provides network access to the user"');
+ add('Distribution layer > "implements network access policy" and "establishes Layer 3 routing boundaries"');
+ add('Core layer > "provides high-speed backbone connectivity" and "functions as an aggregator for all the campus blocks"');
+ add('cut-throught > "appropriate for high perfomance..." and "forwarding process can..." and "may forward invalid frames"');
+ add('store-and-forward > "error checking..." and "forwarding process only..." and "only forwards valid frames"');
