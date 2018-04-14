c08937778e26a5c4beff24d20d1bec966b4def67
Roman Anufriev <mrspeedstalker@gmail.com>
2018-04-14 05:48:44 +0300

    Added CCNA4 v6.0 Chapter 8, deleted CCNA4 v6.0 Chapter 7

--- answers_archive.js
+++ answers_archive.js
@@ -6771,3 +6771,60 @@ add('Which QoS model uses the DSCP bits to mark packets and provides 64 possible
     'DiffServ');
 add('Which QoS technique retains excess packets in a separate queue for later transmission?',
     'shaping');
+
+
+// CCNA4 v6.0 Chapter 7
+add('What is an example of an M2M connection in the IoT?',
+    'Sensors in a warehouse communicate with each other and send data to a server block in the cloud.');
+add('What is the term for the extension of the existing Internet structure to billions of connected devices?',
+    'IoT');
+add('Which statement describes the Cisco IoT System?',
+    'It is an infrastructure to manage large scale systems of very different endpoints and platforms.');
+add('Which three network models are described in the fog computing pillar of the Cisco IoT System? (Choose three.)',
+    'fog computing',
+    'client/server',
+    'cloud computing');
+add('Which IoT pillar extends cloud connectivity closer to the network edge?',
+    'fog computing pillar');
+add('Which cybersecurity solution is described in the security pillar of the Cisco IoT System to address the security of power plants and factory process lines?',
+    'operational technology specific security');
+add('Which cloud computing opportunity would provide the use of network hardware such as routers and switches for a particular company?',
+    'infrastructure as a service (IaaS)');
+add('What technology allows users to access data anywhere and at any time?',
+    'Cloud computing');
+add('The exhibit is not required to answer the question. The exhibit shows a fog covering trees on the side of a mountain.What statement describes Fog computing?',
+    'It creates a distributed computing infrastructure that provides services close to the network edge.');
+add('Which Cloud computing service would be best for a new organization that cannot afford physical servers and networking equipment and must purchase network services on-demand?',
+    'IaaS');
+add('Which cloud model provides services for a specific organization or entity?',
+    'a private cloud');
+add('How does virtualization help with disaster recovery within a data center?',
+    'support of live migration');
+add('What is a difference between the functions of Cloud computing and virtualization?',
+    'Cloud computing separates the application from the hardware whereas virtualization separates the OS from the underlying hardware.');
+add('Which two business and technical challenges does implementing virtualization within a data center help businesses to overcome? (Choose two.)',
+    'physical footprint',
+    'power and air conditioning');
+add('Which statement describes the concept of cloud computing?',
+    'separation of application from hardware');
+add('Which is a characteristic of a Type 2 hypervisor?',
+    'does not require management console software');
+add('Which is a characteristic of a Type 1 hypervisor?',
+    'installed directly on a server');
+add('How is the control plane modified to operate with network virtualization?',
+    'The control plane function is consolidated into a centralized controller.');
+add('Which technology virtualizes the network control plane and moves it to a centralized controller?',
+    'SDN');
+add('Which two layers of the OSI model are associated with SDN network control plane functions that make forwarding decisions? (Choose two.)',
+    'Layer 2',
+    'Layer 3');
+add('What pre-populates the FIB on Cisco devices that use CEF to process packets?',
+    'the routing table');
+add('Which type of hypervisor would most likely be used in a data center?',
+    'Type 1');
+add('What component is considered the brains of the ACI architecture and translates application policies?',
+    'the Application Policy Infrastructure Controller');
+add('In an IoT implementation, devices will be connected to a network to share the same infrastructure and to facilitate communications, analytics, and management.',
+    'converged');
+add('In a scenario where a user with a laptop running the Mac OS installs a Windows virtual OS instance, the user is implementing a Type hypervisor.',
+    '2');
