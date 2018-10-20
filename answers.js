04425a81f28e9c22fe71065729f4ba168e663daf
Eduard Shubert <schubert59@mail.ru>
2018-10-20 04:10:39 +0300

    Updated CCNA3 Scaling Networks v6.0 Chapter 6

--- answers.js
+++ answers.js
@@ -6,64 +6,63 @@ var add = tests.add;
  * add(question, answer0 [, answer1, ...]);
  */
 
-// CCNA3 Scaling Networks v6.0 Chapter 5
-add('Which dynamic routing protocol was developed as an exterior gateway protocol to interconnect different Internet providers?',
-    'BGP');
-add('In the context of routing protocols, what is a definition for time to convergence?',
-    'the amount of time for the routing tables to achieve a consistent state after a topology change');
-add('An OSPF enabled router is processing learned routes to select best paths to reach a destination network. What is the OSPF algorithm evaluating as the metric?',
-    'The cumulative bandwidth that is used along the routing path.');
-add('What is the difference between interior and exterior routing protocols?',
-    'Interior routing protocols are used to communicate within a single autonomous system. Exterior routing protocols are used to communicate between multiple autonomous systems.');
-add('What are two purposes of dynamic routing protocols? (Choose two.)',
-    'discover remote networks',
-    'select best path to destination networks');
-add('Which routing protocol is designed to use areas to scale large hierarchical networks?',
-    'OSPF');
-add('Which two routing protocols are classified as distance vector routing protocols? (Choose two.)',
-    'EIGRP',
-    'RIP');
-add('A network administrator has configured RIPv2 in the given topology. Which path would a packet take to get from the LAN that is connected to R1 to the LAN that is connected to R7?',
-    'R1–R2–R6–R7');
-add('Which routing protocol sends a routing update to neighboring routers every 30 seconds?',
-    'RIP');
-add('After a network topology change occurs, which distance vector routing protocol can send an update message directly to a single neighboring router without unnecessarily notifying other routers?',
-    'EIGRP');
-add('Which feature provides secure routing updates between RIPv2 neighbors?',
-    'routing protocol authentication');
-add('What is maintained within an EIGRP topology table?',
-    'all routes received from neighbors');
-add('A network administrator is researching routing protocols for implementation in a critical network infrastructure. Which protocol uses the DUAL algorithm to provide almost instantaneous convergence during a route failover?',
-    'EIGRP');
-add('OSPF is used in the network. Which path will be chosen by OSPF to send data packets from Net A to Net B?',
-    'R1, R3, R5, R7');
-add('What are two features of the OSPF routing protocol? (Choose two.)',
-    'calculates its metric using bandwidth',
-    'uses Dijkstra’s algorithm to build the SPF tree');
-add('Which two protocols are link-state routing protocols? (Choose two.)',
-    'OSPF',
-    'IS-IS');
-add('Which routing protocol uses link-state information to build a map of the topology for computing the best path to each destination network?',
-    'OSPF');
-add('Which two requirements are necessary before a router configured with a link-state routing protocol can build and send its link-state packets? (Choose two.)',
-    'The router has determined the costs associated with its active links.',
-    'The router has established its adjacencies.');
-add('What happens when two link-state routers stop receiving hello packets from neighbors?',
-    'They consider the neighbor to be unreachable and the adjacency is broken.');
-add('Which two events will trigger the sending of a link-state packet by a link-state routing protocol? (Choose two.)',
-    'a change in the topology',
-    'the initial startup of the routing protocol process');
-add('What is the first step taken by a newly configured OSPF router in the process of reaching a state of convergence?',
-    'It learns about directly connected links in an active state.');
-add('Which two components of an LSP enable an OSPF router to determine if the LSP that is received contains newer information than what is in the current OSPF router link-state database? (Choose two.)',
-    'sequence numbers',
-    'aging informatio');
-add('Which statement is an incorrect description of the OSPF protocol?',
-    'When compared with distance vector routing protocols, OSPF utilizes less memory and less CPU processing power.');
-add('What is a disadvantage of deploying OSPF in a large single area routing environment?',
-    'OSPF uses excessive LSP flooding.');
-add('What is the first step taken by a newly configured OSPF router in the process of reaching a state of convergence?',
-    'It learns about directly connected links in an active state.');
+// CCNA3 Scaling Networks v6.0 Chapter 6
+add('When an EIGRP-enabled router uses a password to accept routes from other EIGRP-enabled routers, which mechanism is used?',
+    'EIGRP authentication');
+add('What is the purpose of using protocol-dependent modules in EIGRP?',
+    'to accommodate routing of different network layer protocols');
+add('If all router Ethernet interfaces in an EIGRP network are configured with the default EIGRP timers, how long will a router wait by default to receive an EIGRP packet from its neighbor before declaring the neighbor unreachable?',
+    '15 seconds');
+add('Which statement describes a characteristic of the delivery of EIGRP update packets?',
+    'EIGRP uses a reliable delivery protocol to send all update packets.');
+add('Which two EIGRP packet types are sent with unreliable delivery? (Choose two.)',
+    'hello',
+    'acknowledgment');
+add('Which destination MAC address is used when a multicast EIGRP packet is encapsulated into an Ethernet frame?',
+    '01-00-5E-00-00-0A');
+add('What is identified within the opcode of an EIGRP packet header?',
+    'the EIGRP message type that is being sent or received from a neighbor');
+add('An administrator issues the router eigrp 100 command on a router. What is the number 100 used for?',
+    'as the autonomous system number');
+add('Why would a network administrator use a wildcard mask in the network command when configuring a router to use EIGRP?',
+    'to exclude some interfaces from the EIGRP process');
+add('What information does EIGRP maintain within the routing table?',
+    'only successors');
+add('Which table is used by EIGRP to store all routes that are learned from EIGRP neighbors?',
+    'the topology table');
+add('Which command or commands must be entered on a serial interface of a Cisco router to restore the bandwidth to the default value of that specific router interface?',
+    'no bandwidth');
+add('Which three metric weights are set to zero by default when costs in EIGRP are being calculated? (Choose three.)',
+    'k2',
+    'k4',
+    'k5');
+add('A new network administrator has been asked to verify the metrics that are used by EIGRP on a Cisco device. Which two EIGRP metrics are measured by using static values on a Cisco device? (Choose two.)',
+    'bandwidth',
+    'delay');
+add('Which two networks contain feasible successors? (Choose two.)',
+    '192.168.71.0',
+    '10.44.100.252');
+add('A network administrator wants to verify the default delay values for the interfaces on an EIGRP-enabled router. Which command will display these values?',
+    'show interfaces');
+add('R3 has two possible paths to the 172.16.99.0 network. What is the reported distance of the feasible successor route?',
+    '2816');
+add('R2 has two possible paths to the 192.168.10.4 network. What would make the alternate route meet the feasibility condition?',
+    'a reported distance less than 3523840');
+add('Which two factors does an EIGRP router use to determine that a route to a remote network meets the feasible condition and is therefore loop-free? (Choose two.)',
+    'the reported distance on a neighbor router',
+    'the feasible distance on the local router');
+add('Which configuration is necessary to ensure successful operation of EIGRP for IPv6?',
+    'The no shutdown command is required within the router configuration mode.');
+add('EIGRP uses the protocol to deliver EIGRP packets to neighbors.',
+    'RTP');
+add('In an EIGRP topology table, a route that is in a/an state will cause the Diffusing Update Algorithm to send EIGRP queries that ask other routers for a path to this network.',
+    'active');
+add('Open the PT Activity. Perform the tasks in the activity instructions and then answer the question. Which code is displayed on the web server?',
+    'Complete');
+add('What protocol is use by EIGRP to deliver EIGRP packets to neighbors?',
+    'RTP');
+add('Which condition will cause DUAL to put an EIGRP route into the active state?',
+    'The successor is no longer available and there is no feasible successor.');
 
 /*Prompts for matching questions and Packet Tracer Labs*/
 add = prompts.add;
@@ -73,5 +72,5 @@ add = prompts.add;
  * add('helper label');
  */
 
-add('adv: event, building, fast');
-add('disadv: bandwidth, memory, CPU');
+add('first > eigrp, second > IPv4 on loopback, third > IPv4 on active');
+add('IPv4 > routable, IPv6 > link-local, both > 32-bit');
