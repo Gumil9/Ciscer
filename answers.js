80c1d7cb7db4ba83072e1f3aa1141a99f4282d00
Eduard Shubert <schubert59@mail.ru>
2018-11-24 02:17:08 +0300

    CCNA3 v6.0 EIGRP Practice Skills Assessment

--- answers.js
+++ answers.js
@@ -6,60 +6,7 @@ var add = tests.add;
  * add(question, answer0 [, answer1, ...]);
  */
 
-// CCNA3 Scaling Networks v6.0 Chapter 10
-add('A network administrator has just changed the router ID on a router that is working in an OSPFv2 environment. What should the administrator do to reset the adjacencies and use the new router ID?',
-    'Issue the clear ip ospf process privileged mode command.');
-add('What three conclusions can be drawn from the displayed output? (Choose three.)',
-    'The DR can be reached through the GigabitEthernet 0/0 interface.',
-    'The router ID values were not the criteria used to select the DR and the BDR.',
-    'There have been 9 seconds since the last hello packet sent.');
-add('Which conclusion can be drawn from this OSPF multiaccess network?',
-    'With an election of the DR, the number of adjacencies is reduced from 6 to 3.');
-add('Which OSPF feature allows a remote OSPF area to participate in OSPF routing when it cannot connect directly to OSPF Area 0?',
-    'virtual link');
-add('What are three resulting DR and BDR elections for the given topology? (Choose three.)',
-    'R1 is DR for segment A.',
-    'R2 is BDR for segment A.',
-    'R5 is BDR for segment B.');
-add('When checking a routing table, a network technician notices the following entry:',
-    'This route is a propagated default route.');
-add('What two conclusions can be drawn based on the output of the show ipv6 route command? (Choose two.)',
-    'R2 receives default route information from another router.',
-    'Route 2001:DB8:CAFE:B001::/64 is a route advertised by an ABR.');
-add('Which command will a network engineer issue to verify the configured hello and dead timer intervals on a point-to-point WAN link between two routers that are running OSPFv2?',
-    'show ip ospf interface serial 0/0/0');
-add('A network engineer suspects that OSPFv3 routers are not forming neighbor adjacencies because there are interface timer mismatches. Which two commands can be issued on the interface of each OSFPv3 router to resolve all timer mismatches? (Choose two.)',
-    'no ipv6 ospf hello-interval',
-    'no ipv6 ospf dead-interval');
-add('A network engineer has manually configured the hello interval to 15 seconds on an interface of a router that is running OSPFv2. By default, how will the dead interval on the interface be affected?',
-    'The dead interval will now be 60 seconds.');
-add('Which command can be used to view the OSPF hello and dead time intervals?',
-    'show ip ospf interface');
-add('A network administrator is configuring OSPF for R1 and R2, but the adjacency cannot be established. What is the cause of the issue?',
-    'The area ID is misconfigured.');
-add('What the amount of time that has elapsed since the router received a hello packet?',
-    '4 seconds');
-add('Which two parameters must match between neighboring OSPF routers in order to form an adjacency? (Choose two.)',
-    'hello / dead timers',
-    'network types');
-add('A network administrator has configured OSPFv2 on the two Cisco routers but PC1 is unable to connect to PC2. What is the most likely problem?',
-    'Interface Fa0/0 has not been activated for OSPFv2 on router R2.');
-add('R1 and R2 are connected to the same LAN segment and are configured to run OSPFv3. They are not forming a neighbor adjacency. What is the cause of the problem?',
-    'The timer intervals of R1 and R2 do not match.');
-add('A network administrator is troubleshooting an OSPFv3 configuration on an IPv6 network. The administrator issues the show ipv6 protocols command. What is the purpose for this command?',
-    'to verify OSPFv3 configuration information');
-add('Directly connected networks configured on router R1 are not being shared with neighboring routers through OSPFv3. What is the cause of the issue?',
-    'There is a mismatch of OSPF process ID in commands.');
-add('An administrator is troubleshooting OSPFv3 adjacency issues. Which command would the administrator use to confirm that OSPFv3 hello and dead intervals are matching between routers?',
-    'show ipv6 ospf interface');
-add('What three states are transient OPSF neighbor states that indicate a stable adjacency is not yet formed between two routers? (Choose three.)',
-    'exstart',
-    'exchange',
-    'loading');
-add('Which routers are the DR and BDR in this topology?',
-    'DR:R3 BDR:R5');
-add('A network administrator is configuring multiarea OSPFv3 on the routers. The routing design requires that the router RT1 is a DROTHER for the network in Area 0 and the DR for the network in Area 1. Check the settings and status of the routers. What can the administrator do to ensure that RT1 will meet the design requirement after all routers restart?',
-    'Use the ipv6 ospf priority 10 command on the interface g0/2 of RT1.');
+
 
 /*Prompts for matching questions and Packet Tracer Labs*/
 add = prompts.add;
@@ -69,4 +16,96 @@ add = prompts.add;
  * add('helper label');
  */
 
-add('1 > interface priority, 2 > manually, 3 > loopback, 4 > physical');
+// CCNA3 v6.0 EIGRP Practice Skills Assessment
+add('CCNA3 v6.0 EIGRP Practice Skills Assessment');
+
+add('DataCenter/Ed_Dept Router = R1 = top-most');
+
+add('Clinic_10/Primary-2 Router = R2-2 = low & left');
+add('Hospital_1/Primary-1 Router = R2 = low & center');
+add('Hospital_2 / High_School Router = R3 = low & right');
+
+add('Net_1 / Dist-1 Switch = S1 = top-most in switch triangle to the right');
+add('FL_1 Switch / Acc-1 Switch = S2 = left-most in switch triangle to the right');
+add('Fl_2 Switch / Acc-2 Switch = S3 = right-most in switch triangle to the right');
+
+add('PC-B = second from the left');
+
+add('PC-A = first from the left');
+add('PC-M = fourth from the left');
+
+add('Lets start!');
+add('DataCenter/Ed_Dept Router = R1 = top-most');
+add('en | conf term | ip route 0.0.0.0 0.0.0.0 s0/1/0 | ipv6 unicast-routing | ipv6 route ::/0 s0/1/0');
+add('router eigrp 99 | network 192.168.100.8 0.0.0.3 | network 192.168.100.20 0.0.0.3 | eigrp router-id 1.1.1.1 | exit');
+add('ipv6 router eigrp 99 | eigrp router-id 1.1.1.1 | no shutdown | exit');
+add('int s0/0/0 | ipv6 eigrp 99 | int s0/0/1 | ipv6 eigrp 99 | int s0/1/0 | ipv6 eigrp 99 | int s0/0/0 | bandwidth 2000 | int s0/0/1 | bandwidth 2000 | exit');
+add('router eigrp 99 | redistribute static | exit | ipv6 router eigrp 99 | redistribute static | exit');
+
+add('Clinic_10/Primary-2 Router = R2-2 = low & left');
+add('en | conf t | ip route 0.0.0.0 0.0.0.0 s0/0/0 | int g0/0 | standby 1 ip 10.1.0.1 | standby version 2');
+
+add('Hospital_1/Primary-1 Router = R2 = low & center');
+add('en | conf t | router eigrp 99 | network 10.1.0.0 0.0.0.255 | network 192.168.100.8 0.0.0.3 | network 192.168.100.32 0.0.0.3 | eigrp router-id 2.2.2.2 | exit');
+add('ipv6 unicast-routing | ipv6 router eigrp 99 | eigrp router-id 2.2.2.2 | no shutdown | exit');
+add('int s0/0/1 | ipv6 eigrp 99 | int s0/0/0 | ipv6 eigrp 99 | int g0/0 | ipv6 eigrp 99 | exit');
+add('int s0/0/0 | bandwidth 2000 | exit | router eigrp 99 | passive-interface g0/0 | exit');
+add('int g0/0 | standby 1 ip 10.1.0.1 | standby 1 preempt | standby 1 priority 150 | standby version 2 | exit');
+
+add('Hospital_2 / High_School Router = R3 = low & right');
+add('en | conf t | router eigrp 99 | network 10.10.0.0 0.0.0.255 | network 10.15.0.0 0.0.0.255 | network 10.20.0.0 0.0.0.255');
+add('network 192.168.100.20 0.0.0.3 | network 192.168.100.32 0.0.0.3 | eigrp router-id 3.3.3.3 | exit');
+add('ipv6 unicast-routing | ipv6 router eigrp 99 | eigrp router-id 3.3.3.3 | no shutdown | exit');
+add('int g0/0.10 | ipv6 eigrp 99 | int g0/0.15 | ipv6 eigrp 99 | int g0/0.20 | ipv6 eigrp 99 | exit');
+add('int s0/0/0 | ipv6 eigrp 99 | int s0/0/1 | ipv6 eigrp 99 | int s0/1/0 | ipv6 eigrp 99 | int s0/0/1 | bandwidth 2000 | exit');
+add('router eigrp 99 | passive-interface g0/0.10 | passive-interface g0/0.15 | passive-interface g0/0.20 | exit');
+add('int g0/0 | no shutdown | exit');
+
+add('Net_1 / Dist-1 Switch = S1 = top-most in switch triangle to the right');
+add('WARN: Change VTP domain and VTP Password to match your exam!');
+add('en | conf t | int range f0/3-4 | channel-group 1 mode active | int range f0/5-6 | channel-group 2 mode active | exit');
+add('int g0/1 | no shutdown | int g0/1 | switchport mode trunk | switchport trunk native vlan 99 | int g0/1 | switchport nonegotiate');
+add('int range f0/3-4 | switchport mode trunk | switchport trunk native vlan 99');
+add('int port-channel 1 | switchport mode trunk | switchport trunk native vlan 99 | switchport nonegotiate');
+add('int range f0/5-6 | switchport mode trunk | switchport trunk native vlan 99');
+add('int port-channel 2 | switchport mode trunk | switchport trunk native vlan 99 | switchport nonegotiate | exit');
+add('vtp mode server | WARN: change domain & password | vtp domain TownEd | vtp password TWE_123! | vtp version 2');
+add('spanning-tree mode rapid-pvst | spanning-tree vlan 10,15,20 priority 24576');
+
+add('FL_1 Switch / Acc-1 Switch = S2 = left-most in switch triangle to the right');
+add('WARN: Change VTP domain and VTP Password to match your exam!');
+add('en | conf t | int range f0/3-4 | channel-group 1 mode active | int range f0/1-2 | channel-group 3 mode active | exit');
+add('int range f0/3-4 | switchport mode trunk | switchport trunk native vlan 99');
+add('int port-channel 1 | switchport mode trunk | switchport trunk native vlan 99 | switchport nonegotiate');
+add('int range f0/1-2 | switchport mode trunk | switchport trunk native vlan 99');
+add('int port-channel 3 | switchport mode trunk | switchport trunk native vlan 99 | switchport nonegotiate | exit');
+add('vtp mode client | WARN: change domain & password | vtp domain TownEd | vtp password TWE_123! | vtp version 2');
+add('spanning-tree mode rapid-pvst | int range f0/10-24 | spanning-tree bpduguard enable | spanning-tree portfast');
+
+add('Fl_2 Switch / Acc-2 Switch = S3 = right-most in switch triangle to the right');
+add('WARN: Change VTP domain and VTP Password to match your exam!');
+add('en | conf t | int range f0/5-6 | channel-group 2 mode active | int range f0/1-2 | channel-group 3 mode active | exit');
+add('int range f0/5-6 | switchport mode trunk | switchport trunk native vlan 99');
+add('int port-channel 2 | switchport mode trunk | switchport trunk native vlan 99 | switchport nonegotiate');
+add('int range f0/1-2 | switchport mode trunk | switchport trunk native vlan 99');
+add('int port-channel 3 | switchport mode trunk | switchport trunk native vlan 99 | switchport nonegotiate | exit');
+add('vtp mode client | WARN: change domain & password | vtp domain TownEd | vtp password TWE_123!');
+add('spanning-tree mode rapid-pvst | spanning-tree vlan 10,15,20 priority 28672');
+
+add('NOTE: you may need to add & delete a vlan on "Net_1 / Dist-1 Switch = S1 = top-most in switch triangle to the right" in order to update config');
+add('vlan 666 | no vlan 666');
+
+add('PC-B = second from the left');
+add('Desktop tab -> Command Prompt');
+add('Enter telnet 2001:DB8:E::1 | Authenticate your Telnet session with the password cisco | Move to privileged EXEC mode using the password admin');
+add('en | conf t | int g0/0 | no sh');
+
+add('PC-A = first from the left');
+add('Desktop tab -> IP configuration');
+add('Default Gateway 10.1.0.1');
+
+add('PC-M = fourth from the left');
+add('Desktop tab -> IP configuration');
+add('Default Gateway 10.1.0.1');
+
+add('That is all!');
