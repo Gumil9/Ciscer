6161d27dbc0dda4287c4f4cadcb99d372ad754cf
Eduard Shubert <schubert59@mail.ru>
2019-04-20 11:57:33 +0300

    CCNA4 v6.0 Chapter 7

--- answers.js
+++ answers.js
@@ -6,7 +6,61 @@ var add = tests.add;
  * add(question, answer0 [, answer1, ...]);
  */
 
-
+// CCNA4 v6.0 Chapter 7
+add('What is an example of an M2M connection in the IoT?',
+    'Sensors in a warehouse communicate with each other and send data to a server block in the cloud.');
+add('What is the term for the extension of the existing Internet structure to billions of connected devices?',
+    'IoT');
+add('Which statement describes the Cisco IoT System?',
+    'It is an infrastructure to manage large scale systems of very different endpoints and platforms.');
+add('Which three network models are described in the fog computing pillar of the Cisco IoT System? (Choose three.)',
+    'fog computing',
+    'client/server',
+    'cloud computing');
+add('Which IoT pillar extends cloud connectivity closer to the network edge?',
+    'fog computing pillar');
+add('Which cybersecurity solution is described in the security pillar of the Cisco IoT System to address the security of power plants and factory process lines?',
+    'operational technology specific security');
+add('Which cloud computing opportunity would provide the use of network hardware such as routers and switches for a particular company?',
+    'infrastructure as a service (IaaS)');
+add('What technology allows users to access data anywhere and at any time?',
+    'Cloud computing');
+add('The exhibit is not required to answer the question. The exhibit shows a fog covering trees on the side of a mountain.What statement describes Fog computing?',
+    'It creates a distributed computing infrastructure that provides services close to the network edge.');
+add('Which Cloud computing service would be best for a new organization that cannot afford physical servers and networking equipment and must purchase network services on-demand?',
+    'IaaS');
+add('Which cloud model provides services for a specific organization or entity?',
+    'a private cloud');
+add('How does virtualization help with disaster recovery within a data center?',
+    'support of live migration');
+add('What is a difference between the functions of Cloud computing and virtualization?',
+    'Cloud computing separates the application from the hardware whereas virtualization separates the OS from the underlying hardware.');
+add('Which two business and technical challenges does implementing virtualization within a data center help businesses to overcome? (Choose two.)',
+    'physical footprint',
+    'power and air conditioning');
+add('Which statement describes the concept of cloud computing?',
+    'separation of application from hardware');
+add('Which is a characteristic of a Type 2 hypervisor?',
+    'does not require management console software');
+add('Which is a characteristic of a Type 1 hypervisor?',
+    'installed directly on a server');
+add('How is the control plane modified to operate with network virtualization?',
+    'The control plane function is consolidated into a centralized controller.');
+add('Which technology virtualizes the network control plane and moves it to a centralized controller?',
+    'SDN');
+add('Which two layers of the OSI model are associated with SDN network control plane functions that make forwarding decisions? (Choose two.)',
+    'Layer 2',
+    'Layer 3');
+add('What pre-populates the FIB on Cisco devices that use CEF to process packets?',
+    'the routing table');
+add('Which type of hypervisor would most likely be used in a data center?',
+    'Type 1');
+add('What component is considered the brains of the ACI architecture and translates application policies?',
+    'the Application Policy Infrastructure Controller');
+add('In an IoT implementation, devices will be connected to a network to share the same infrastructure and to facilitate communications, analytics, and management.',
+    'converged');
+add('In a scenario where a user with a laptop running the Mac OS installs a Windows virtual OS instance, the user is implementing a Type hypervisor.',
+    '2');
 
 /*Prompts for matching questions and Packet Tracer Labs*/
 add = prompts.add;
@@ -16,57 +70,4 @@ add = prompts.add;
  * add('helper label');
  */
 
-// CCNA4 v6.0 Chapter 5: PT Skills Assessment - SIC: Access Control List and SNMP Configuration
-add('1. HQ / Central (?) / Main (?) Router');
-add('2. en');
-add('3. conf t');
-add('4. snmp-server community hq-monitor ro');
-add('5. snmp-server community hq-inside rw');
 
-add('6. access-list 1 permit 192.168.10.0 0.0.0.255');
-add('7. access-list 12 permit host 192.168.10.5');
-add('8. line vty 0 1');
-add('9. access-class 12 in');
-add('10. exit');
-
-add('11. ip access-list extended SNMPACCESS');
-add('12. Permit udp host 192.168.10.5 host 192.168.10.1 eq 161');
-add('13. deny udp any host 192.168.10.1 eq 161');
-add('14. permit ip any any');
-add('15. interface g0/0');
-add('16. ip access-group SNMPACCESS in');
-add('17. exit');
-
-add('18. ip access-list extended SNMPDENY');
-add('19. deny udp any host 172.16.10.1 eq 161');
-add('20. permit ip any any');
-add('21. interface g0/1');
-add('22. ip access-group SNMPDENY IN');
-add('23. exit');
-
-add('24. ip access-list extended INTOHQ');
-add('25. permit tcp any host 172.16.10.5 eq 53');
-add('26. permit udp any host 172.16.10.5 eq 53');
-add('27. permit tcp any host 172.16.10.10 eq 80');
-add('28. permit tcp any any established');
-add('29. interface s0/0/0');
-add('30. ip access-group INTOHQ IN');
-add('31. exit');
-
-add('32. ip access-list extended IN-DMZ');
-add('33. permit udp any host 172.16.10.5 eq 53');
-add('34. permit tcp any host 172.16.10.5 eq 53');
-add('35. permit tcp any host 172.16.10.10 eq 80');
-add('36. permit tcp host 192.168.10.5 host 172.16.10.10 eq 20');
-add('37. permit tcp host 192.168.10.5 host 172.16.10.10 eq 21');
-add('38. interface g0/1');
-add('39. ip access-group IN-DMZ out');
-add('40. exit');
-
-add('41. ipv6 access-list DMZFTP');
-add('42. deny udp any host 2001:DB8:ABCD:E::1 eq 161');
-add('43. permit tcp host 2001:DB8:ABCD:B::5 host 2001:DB8:ABCD:A::10 eq 20');
-add('44. permit tcp host 2001:DB8:ABCD:B::5 host 2001:DB8:ABCD:A::10 eq 21');
-add('45. permit tcp 2001:DB8:ABCD:B::/64 host 2001:DB8:ABCD:A::10 eq 80');
-add('46. int s0/0/1');
-add('47. ipv6 traffic-filter DMZFTP IN');
