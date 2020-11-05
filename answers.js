4a86c9c0845dff596d17acee30f594937c1691d8
Nikolai Gaiduchenko <Gaiduchenko.NE@gmail.com>
2020-11-05 16:53:49 +0300

    mini-testing 4

--- answers.js
+++ answers.js
@@ -7,10 +7,17 @@ var add = tests.add;
  */
 
 // CCNA4 v7.0 test modules 1-4
-add('Which tasks can be accomplished by using the command history feature? (Choose two.)', 'Set the command history buffer size.');
+d('Which tasks can be accomplished by using the command history feature? (Choose two.)', 'Set the command history buffer size.', 'Recall previously entered commands.');
 add('What is the first action in the boot sequence when a switch is powered on?', 'load a power-on self-test program');
 add('What must an administrator have in order to reset a lost password on a router?', 'physical access to the router');
-
+add('When configuring a switch for SSH access, what other command that is associated with the login local command is required to be entered on the switch?', 'username username secret secret');
+add('Which command displays information about the auto-MDIX setting for a specific interface?​', 'show controllers');
+add('If one end of an Ethernet connection is configured for full duplex and the other end of the connection is configured for half duplex, where would late collisions be observed?', 'on the half-duplex end of the connection');
+add('Which command is used to set the BOOT environment variable that defines where to find the IOS image file on a switch?', 'boot system');
+add('What does a switch use to locate and load the IOS image?', 'BOOT environment variable');
+add('Which protocol adds security to remote connections?', 'SSH');
+add('What is a characteristic of an IPv4 loopback interface on a Cisco IOS router?​', 'It is a logical interface internal to the router.');
+add('What is the minimum Ethernet frame size that will not be discarded by the receiver as a runt frame?', '64 bytes');
 
 /*Prompts for matching questions and Packet Tracer Labs*/
 add = prompts.add;
